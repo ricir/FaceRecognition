f13560c37ea726c2f652416927c9678558381c4b
ricir <ricir@ORMARIGA-G>
2023-02-23 14:55:57 +0100

    See readme.md for changes in this commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+This project is forked from ashima0109/FaceRecognition.
+This version adds the possibility to create a clean copy of all the images where a match was found, together with a csv file with the list of the files.
+This version automatically skips non- image files (files with extension different from jpg, jpeeg, gif, png) without stopping for error.
+A file (test3) with no matches for test purposes is included.
--- recognize.py
+++ recognize.py
@@ -1,11 +1,11 @@
-# Usage: python3 recognize.py -i <path_to_test_image_dir> -o <path_to_output_dir>
+# Usage: python3 recognize.py -i <path_to_test_image_dir> -o <path_to_output_dir> -c <path_to_collect_dir> [-l ]
 
 import os, sys
 import cv2 
 from imutils import paths
 import pickle
 import face_recognition
-import argparse
+import argparse, shutil
 
 # load the reference encodings created in the script album.py
 data = pickle.loads(open('friends_face_encodings.pickle', "rb").read())
@@ -13,66 +13,92 @@ data = pickle.loads(open('friends_face_encodings.pickle', "rb").read())
 # make the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 
-# provide a path to the directory containing test images and 
-# a path to the directory where you would like to save your output data
+# provide:
+# a path to the directory containing test images and 
+# a path to the directory where you would like to save your output data and
+# a path to the directory where you would like to save clean copies of the images where a match was found
+# set the limit option if you want only matched images in the output directory
 ap.add_argument("-i", "--test_directory", required=True,
 	help="path to the test image directory")   
 ap.add_argument("-o", "--output_directory", required=True,
-	help="path to the output directory")        
+	help="path to the output directory")
+ap.add_argument("-c", "--collect_directory", required=True,
+	help="path to the collect directory")
+ap.add_argument ('-l', "--limit", action = "store_true", dest = "OnlyMatched",
+	help="limit files copied to output directory only to files with a match")
 args = vars(ap.parse_args())
+
 
 test_dir = args["test_directory"]
 output_dir = args["output_directory"]
+collect_dir = args["collect_directory"]
+OnlyMatched = args["OnlyMatched"]
+
+#  Open for write a file which will contain a list of the files with matched faces
+ListFiles = collect_dir+"/matched.csv"
+x = open (ListFiles, "w")
 
 # initialize a map linking the faces and the filenames they are found in the output
 filemap = {names: [] for names in data["names"]}
 
-# loop over all the images in the test directory
-for count, image in enumerate(os.listdir(test_dir)):
-   
-    imagepath = os.path.join(test_dir, image)
-    filename = imagepath.split(os.path.sep)[-1]
+# loop over all the images in the test directory tree
+# for count, image in enumerate(os.listdir(test_dir)):
+for subdir, dirs, files in os.walk(test_dir):
+    for filename in sorted(files):
+        imagepath = os.path.join(subdir, filename)   
+        filename = imagepath.split(os.path.sep)[-1]
+        
+        dummy, file_extension = os.path.splitext(filename)
 
-    # load the image
-    testimage = cv2.imread(imagepath)
+        # Skip non image files
+        if(file_extension.lower() in [".png",".jpg",".jpeg",".gif" ]):
 
-    # extract the position of bounding box of the face and their corresponding face encodings
-    bboxes = face_recognition.face_locations(testimage, model='hog')
-    encodings = face_recognition.face_encodings(testimage, bboxes)
+            # load the image
+            testimage = cv2.imread(imagepath)
 
-    names = []
+            # extract the position of bounding box of the face and their corresponding face encodings
+            bboxes = face_recognition.face_locations(testimage, model='hog')
+            encodings = face_recognition.face_encodings(testimage, bboxes)
 
-    # loop over the found encodings and compare it to the encodings in the reference database
-    for encoding in encodings:
+            names = []
+            save = False
+            # loop over the found encodings and compare it to the encodings in the reference database
+            for encoding in encodings:
 
-        matches = face_recognition.compare_faces(data["encodings"], encoding)
-        name = "Unknown"
+                matches = face_recognition.compare_faces(data["encodings"], encoding)
+                name = "Unknown"
 
-        # if the test image has even a single face that matched a face in the database
-        if True in matches:
+                # if the test image has even a single face that matched a face in the database
+                if True in matches:
 
-            # extract the matched indices 
-            matchedIdxs = [i for (i, b) in enumerate(matches) if b]
-            counts = {}
+                    # extract the matched indices 
+                    matchedIdxs = [i for (i, b) in enumerate(matches) if b]
+                    counts = {}
 
-            # extract the corresponding names of the matched indices and get a vote count for each matched face name 
-            for i in matchedIdxs:
-                name = data["names"][i]
-                counts[name] = counts.get(name, 0) + 1
+                    # extract the corresponding names of the matched indices and get a vote count for each matched face name 
+                    for i in matchedIdxs:
+                        name = data["names"][i]
+                        counts[name] = counts.get(name, 0) + 1
 
-            # the name of the face with maximum number of votes wins
-            name = max(counts, key=counts.get)
+                    # the name of the face with maximum number of votes wins
+                    name = max(counts, key=counts.get)
+                    save = True
 
-        names.append(name)   
+                names.append(name)
+            # Copuy the matched file to collect_dir
+            if save:
+                shutil.copy2(imagepath, collect_dir)
+                x.write(imagepath+'\n')
+                
+                
+            # draw the bounding box around the faces with their detected names
+            for ((top, right, bottom, left), name) in zip(bboxes, names):
 
-    # draw the bounding box around the faces with their detected names
-    for ((top, right, bottom, left), name) in zip(bboxes, names):
-
-        cv2.rectangle(testimage, (left, top), (right, bottom), (0, 255, 0), 2)
-        cv2.putText(testimage, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-        if name != 'Unknown':
-            filemap[name].append(filename)
-
-    outputname = "output_%s" % filename
-    outputpath = os.path.join(output_dir, outputname)
-    cv2.imwrite(outputpath, testimage)
+                cv2.rectangle(testimage, (left, top), (right, bottom), (0, 255, 0), 2)
+                cv2.putText(testimage, name, (left, top - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+                if name != 'Unknown':
+                    filemap[name].append(filename)
+            if (not OnlyMatched or save): 
+                outputname = "output_%s" % filename
+                outputpath = os.path.join(output_dir, outputname)
+                cv2.imwrite(outputpath, testimage)
